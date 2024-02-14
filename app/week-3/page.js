7fdd2e0eb4975f372b0d42be3fd761e3e50d77f1
lucasdayan <lucas.dayan4@gmail.com>
2024-02-13 21:58:52 -0700

    Add week 3 assignment

---
+++ app/week-3/page.js
@@ -0,0 +1,12 @@
+
+import Item from './item.js';
+import ItemList from './item-list.js';
+
+export default function Page() {
+    return (
+        <main>
+            <h1 class = "text-3xl font-bold m-2">Shopping List</h1>
+            <ItemList></ItemList>    
+        </main>
+    )
+}
