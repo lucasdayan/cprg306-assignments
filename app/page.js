6187d88d8d9fb930e44e930fdafe8441a3940dd1
lucasdayan <lucas.dayan4@gmail.com>
2024-02-13 22:47:49 -0700

    week 4 assignment added

--- app/page.js
+++ app/page.js
@@ -53,6 +53,23 @@ export default function Home() {
             Click here to navigate to week 3's assignment
           </p>
         </a>
+
+        <a
+          href="/week-4"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Week 4{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            Click here to navigate to week 4's assignment
+          </p>
+        </a>
       </div>
     </main>
   );
